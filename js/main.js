31b1c20fb5fd9befcff964b677982b3cf94c62ee
Ambiya Nelwan <ambiya.nelwan@gmail.com>
2021-09-08 21:02:38 +0700

    pasrah

--- js/main.js
+++ js/main.js
@@ -14,25 +14,10 @@ function removeItem() {
     var item = document.getElementById(candidate.value);
     ul.removeChild(item);
 }
-function addItem1() {
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
-    var li = document.createElement("li");
-    li.setAttribute('id', candidate.value);
-    li.appendChild(document.createTextNode(candidate.value));
-    ul.appendChild(li);
-}
 
-
-function removeItem1() {
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
-    var item = document.getElementById(candidate.value);
-    ul.removeChild(item);
-}
 function addItem2() {
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
+    var ul = document.getElementById("dynamic-list2");
+    var candidate = document.getElementById("candidate2");
     var li = document.createElement("li");
     li.setAttribute('id', candidate.value);
     li.appendChild(document.createTextNode(candidate.value));
@@ -41,9 +26,10 @@ function addItem2() {
 
 
 function removeItem2() {
-    var ul = document.getElementById("dynamic-list");
-    var candidate = document.getElementById("candidate");
+    var ul = document.getElementById("dynamic-list2");
+    var candidate = document.getElementById("candidate2");
     var item = document.getElementById(candidate.value);
     ul.removeChild(item);
 }
+
 
